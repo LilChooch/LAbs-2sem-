87ac2ad44be276a55608ec4b1d506a4fbb46475a
LilChooch <oleg.2018.85.ru@gmail.com>
2024-05-30 13:56:33 +0500

    Laba_6

---
+++ RpnWPF3/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using RpnLogic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RpnWPF3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btClick_Click(object sender, RoutedEventArgs e)
+        {
+            lbOutput.Content = new ExpRpn(tbInput.Text).Value;
+        }
+    }
+}
