585456df2f8d234eea9d9bab78c3c5e79e21dd61
LilChooch <oleg.2018.85.ru@gmail.com>
2024-06-04 15:45:24 +0500

    lab8

---
+++ RpnWPF3/CanvasDrawer.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using RpnLogic;
+using System.Windows.Documents;
+using System.CodeDom;
+using System.Xml.Linq;
+
+
+
+
+
+namespace RpnWPF3
+{
+
+    static class PointExtensions
+    {
+        public static Point ToMathCoordinates(this Point point, Canvas canvas, float zoom)
+        {
+            return new Point((point.X - canvas.ActualWidth / 2) / zoom, (canvas.ActualHeight / 2 - point.Y) / zoom);
+        }
+
+        public static Point ToUiCoordinates(this Point point, Canvas canvas, float zoom)
+        {
+            return new Point((point.X * zoom + canvas.ActualWidth / 2), (canvas.ActualHeight / 2 - point.Y * zoom));
+        }
+    }
+
+
+
+    public class CanvasDrawer
+    {
+        private readonly Canvas _canvas;
+        private readonly double _start;
+        private readonly double _end;
+        private readonly double dashSize = 5;
+        private readonly double _step;
+        private readonly float _scale;
+        private readonly Brush _lineGraph = Brushes.Blue;
+        private readonly Brush _pointBrush = Brushes.Red;
+        private readonly Point _lineStartX, _lineEndX, _lineStartY, _lineEndY;
+
+        public CanvasDrawer(Canvas canvas, double startX, double endX, double step, float scale)
+        {
+            _canvas = canvas;
+
+            _lineStartX = new Point(_canvas.ActualWidth / 2, 0);
+            _lineEndX = new Point(_canvas.ActualWidth / 2, (int)_canvas.ActualHeight);
+
+            _lineStartY = new Point(0, _canvas.ActualHeight / 2);
+            _lineEndY = new Point(_canvas.ActualWidth, _canvas.ActualHeight / 2);
+
+            _scale = scale;
+            _step = step;
+            _start = startX;
+            _end = endX;
+        }
+
+        public void DrawLine(Point startPoint, Point endPoint, Brush stroke = null)
+        {
+            stroke ??= _lineGraph;
+            Line line = new Line
+            {
+                X1 = startPoint.X,
+                Y1 = startPoint.Y,
+                X2 = endPoint.X,
+                Y2 = endPoint.Y,
+                Stroke = stroke,
+                StrokeThickness = 1
+            };
+
+            _canvas.Children.Add(line);
+        }
+
+        public void DrawAxes()
+        {
+            DrawLine(_lineStartX, _lineEndX, _lineGraph);
+            DrawLine(_lineStartY, _lineEndY, _lineGraph);
+        }
+
+        public void DrawGraph(List<Point> points)
+        {
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                var uiPoint1 = points[i].ToUiCoordinates(_canvas, _scale);
+                var uiPoint2 = points[i + 1].ToUiCoordinates(_canvas, _scale);
+                DrawLine(uiPoint1, uiPoint2);
+                DrawPoint(uiPoint1);
+                DrawDashLine(points[i]);
+            }
+            if (points.Count > 0)
+            {
+                var lastUiPoint = points[points.Count-1].ToUiCoordinates(_canvas, _scale);
+                DrawPoint(lastUiPoint);
+                DrawDashLine(points[points.Count - 1]);
+            }
+        }
+
+        public void DrawDashLine(Point point)
+        {
+            
+            var uiPoint = point.ToUiCoordinates(_canvas, _scale);
+            DrawLine(new Point(uiPoint.X, _canvas.ActualHeight / 2 - dashSize), new Point(uiPoint.X, _canvas.ActualHeight / 2 + dashSize));
+            DrawLine(new Point(_canvas.ActualWidth / 2 - dashSize, uiPoint.Y), new Point(_canvas.ActualWidth / 2 + dashSize, uiPoint.Y));
+        }
+        public void DrawPoint(Point point, double size = 4)
+        {
+            Ellipse ellipse = new Ellipse
+            {
+                Width = size,
+                Height = size,
+                Fill = _pointBrush
+            };
+
+            Canvas.SetLeft(ellipse, point.X - size / 2);
+            Canvas.SetTop(ellipse, point.Y - size / 2);
+
+            _canvas.Children.Add(ellipse);
+        }
+       
+
+       
+
+
+    }
+}
+
+
+
+
+
+
+
--- RpnWPF3/MainWindow.xaml.cs
+++ RpnWPF3/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using RpnLogic;
+using System.Globalization;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,10 +23,51 @@ namespace RpnWPF3
             InitializeComponent();
         }
 
-        private void btClick_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            double X = double.Parse(tbX.Text);
-            lbOutput.Content = new ExpRpn(tbInput.Text,X).Value;
+            CanvasGraph.Children.Clear();
+            
+            DrawCanva();
         }
+        private void DrawCanva()
+        {
+            string input = expressoiN.Text;
+            float scale = float.Parse(tbscale.Text);
+            double start = double.Parse(tbstart.Text);
+            double end = double.Parse(tbend.Text);
+            double step = double.Parse(tbstep.Text);
+            var canvsGraph = CanvasGraph;
+
+            var canvDrawer = new CanvasDrawer(canvsGraph, start, end, step, scale);
+            canvDrawer.DrawAxes();
+
+            List<Point> points = new List<Point>();
+            for(double x = start; x <= end; x+=step)
+            {
+                var y = new ExpRpn(input, x).Value;
+                points.Add(new Point(x, y));
+            }
+            canvDrawer.DrawGraph(points);
+
+        }
+
+        private void CanvasGraph_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point uiPoint = Mouse.GetPosition(CanvasGraph);
+            float scale;
+            if (string.IsNullOrWhiteSpace(tbscale.Text) || !float.TryParse(tbscale.Text, out scale))
+            {
+                scale = 1.0f;
+                tbscale.Text = scale.ToString();
+            }
+
+            var mathPoint = uiPoint.ToMathCoordinates(CanvasGraph, scale);
+            lbUiPoint.Content = $"{uiPoint.X:0.#};{uiPoint.Y:0.#}";
+            lbMathPoint.Content = $"{mathPoint.X:0.#};{mathPoint.Y:0.#}";
+
+
+        }
+
+       
     }
 }
