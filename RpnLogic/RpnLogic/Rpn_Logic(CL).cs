b9fcf80592653826d32252d5f5adcd6791098f14
LilChooch <oleg.2018.85.ru@gmail.com>
2024-06-06 01:58:58 +0500

    lab9

--- RpnLogic/RpnLogic/Rpn_Logic(CL).cs
+++ RpnLogic/RpnLogic/Rpn_Logic(CL).cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using static System.Net.Mime.MediaTypeNames;
 
+
 namespace RpnLogic
 {
 
@@ -46,6 +47,12 @@ namespace RpnLogic
                 else if (IsOperator(currentChar) || IsParenthesis(currentChar))
                 {
                     ProcessOperatorOrParenthesis(currentChar, rpn, stack);
+                }
+                else if (IsFunction(currentChar, input, i, out int newIndex))
+                {
+                    string functionName = GetFunctionName(input, i, out newIndex);
+                    stack.Push(new Function(functionName));
+                    i = newIndex;
                 }
             }
             while (stack.Count > 0)
@@ -99,6 +106,17 @@ namespace RpnLogic
                     double operand1 = numbers.Pop();
                     numbers.Push(operatorToken.Apply(operand1, operand2));
                 }
+                else if (token is Function functionToken)
+                {
+                    List<double> operands = new List<double>();
+
+                    for (int i = 0; i < functionToken.FuncOperand; i++)
+                    {
+                        operands.Insert(0, numbers.Pop());
+                    }
+
+                    numbers.Push(functionToken.ApplyFunc(operands.ToArray()));
+                }
             }
 
             if (numbers.Count == 1)
@@ -127,6 +145,21 @@ namespace RpnLogic
         {
             return symbol == '(' || symbol == ')';
         }
+        private bool IsFunction(char symbol, string input, int index, out int newIndex)
+        {
+            string functionName = GetFunctionName(input, index, out newIndex);
+            return Function.FunctionNames.Contains(functionName);
+        }
+
+        private string GetFunctionName(string input, int startIndex, out int newIndex)
+        {
+            newIndex = startIndex;
+            while (newIndex + 1 < input.Length && char.IsLetter(input[newIndex + 1]))
+            {
+                newIndex++;
+            }
+            return input.Substring(startIndex, newIndex - startIndex + 1);
+        }
 
         private int GetPriority(char symbol)
         {
@@ -134,6 +167,7 @@ namespace RpnLogic
             {
                 case '+':
                 case '-':
+                case '^':
                     return 1;
                 case '*':
                 case '/':
@@ -142,6 +176,7 @@ namespace RpnLogic
                     return 0;
             }
         }
+       
     }
 
     class Token
@@ -182,7 +217,7 @@ namespace RpnLogic
                 }
             }
         }
-
+      
         public double Apply(double operand1, double operand2)
         {
             switch (Symbol)
@@ -193,6 +228,8 @@ namespace RpnLogic
                     return operand1 - operand2;
                 case '*':
                     return operand1 * operand2;
+                case '^':
+                    return Math.Pow(operand1, operand2);
                 case '/':
                     if (operand2 != 0)
                     {
@@ -206,9 +243,67 @@ namespace RpnLogic
                     throw new ArgumentException("Недопустимый оператор " + Symbol);
             }
         }
+       
+
+    }
+    class Function : Token
+    {
+        public static readonly List<string> FunctionNames = new List<string>
+        {
+        "log", "sqrt", "rt", "sin", "cos", "tg", "ctg"
+        };
+
+        public Function(string name) : base(name[0]) { Name = name; }
+
+        public string Name { get; }
+
+        public int FuncOperand
+        {
+            get
+            {
+                switch (Name)
+                {
+                    case "log":
+                    case "rt":
+                        return 2;
+                    case "sqrt":
+                    case "sin":
+                    case "cos":
+                    case "tg":
+                    case "ctg":
+                        return 1;
+                    default:
+                        throw new ArgumentException("Недопустимая функция " + Name);
+                }
+            }
+        }
+        public double ApplyFunc(double[] operands)
+        {
+            switch (Name)
+            {
+                case "log":
+                    return Math.Log(operands[1], operands[0]);
+                case "sqrt":
+                    return Math.Sqrt(operands[0]);
+                case "rt":
+                    return Math.Pow(operands[1], 1.0 / operands[0]);
+                case "sin":
+                    return Math.Sin(operands[0]);
+                case "cos":
+                    return Math.Cos(operands[0]);
+                case "tg":
+                    return Math.Tan(operands[0]);
+                case "ctg":
+                    return 1.0 / Math.Tan(operands[0]);
+                default:
+                    throw new ArgumentException("Недопустимая функция " + Name);
+            }
+        }
     }
 
-    class LeftParenthesis : Token
+
+
+        class LeftParenthesis : Token
     {
         public LeftParenthesis() : base('(') { }
     }
